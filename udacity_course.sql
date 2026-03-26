52698706dc1f3b3170e8d54482867966f987ed4d
Etinista <121003613+Etinista@users.noreply.github.com>
2026-03-25 23:28:44 -0500

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -2,3 +2,20 @@
 Select *
 From  Accounts
 limit 10;
+-- ORDER BY PRACTICE
+SELECT   id, account_id, total_amt_usd
+FROM      orders
+ORDER BY  total_amt_usd DESC
+LIMIT     5
+-- WHERE (goes before "ORDER BY AND LIMIT CLAUSES)
+SELECT  *
+FROM  orders
+WHERE gloss_amt_usd >= 1000
+LIMIT  5;
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil'
+
+-- Arithmetic Operators (PEMDAS is the standard order of operations)
+
