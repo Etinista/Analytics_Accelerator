806a43042a7854790cc9c4ba2adfb858e5713ece
Etinista <121003613+Etinista@users.noreply.github.com>
2026-04-28 21:52:11 -0500

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -19,3 +19,30 @@ WHERE name = 'Exxon Mobil'
 
 -- Arithmetic Operators (PEMDAS is the standard order of operations)
 
+--SELECT standard_amt_usd, standard_qty, standard_amt_usd/standard_qty as unit_price, id, account_id
+--FROM  orders
+--LIMIT 10
+
+SELECT poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) *100 as poster_percentage_revenue, id, account_id
+FROM orders
+LIMIT 10
+
+ --LIKE OPERATOR
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%'
+
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%' OR name LIKE '%one%' OR name LIKE '%s'
+
+  -- IN Operator
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom')
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+
+--REMEMBER TO END QUERY WITH A SEMICOLON!! Standard industry practice
